4d350f5ad31aefa2d45c6067a729232fa96f0da4
Alex <betafoundt@gmail.com>
2022-10-11 14:02:46 +0200

    deploy to heroku healthcheck

--- app.js
+++ app.js
@@ -11,3 +11,11 @@ app.use(express.static('dist'))
 app.listen(PORT, () => {
   console.log('server started on port 5000')
 })
+
+app.get('/health', (_req, res) => {
+  res.send('ok')
+})
+
+app.get('/version', (_req, res) => {
+  res.send('1') // change this string to ensure a new version deployed
+})
